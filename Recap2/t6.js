a0f20e51907ee089ce8841bdb33ac949fbbefec0
Khaled <khaled-marai1@hotmail.com>
2026-03-18 11:45:07 +0200

    Lisää t6 elokuvien syöttö, lajittelu ja näyttö
    * Kysyy käyttäjältä elokuvien lukumäärän
    * Kerää elokuvien nimet ja arvosanat
    * Tallentaa elokuvat olio-taulukkoon
    * Lajittelee elokuvat arvosanan mukaan (paras ensin)
    * Näyttää kaikki elokuvat listana sivulla
    * Korostaa parhaan elokuvan suosikiksi

---
+++ Recap2/t6.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const numOfMovies = prompt('Syötä elokuvien lukumäärä:');
+
+const movies = [];
+
+for (let i = 0; i < numOfMovies; i++) {
+  const nimi = prompt('Syötä elokuvan nimi:');
+  const arvosana = prompt('Syötä elokuvan arvosana:');
+  movies.push({
+    nimi: nimi,
+    arvosana: arvosana,
+  });
+}
+
+movies.sort(function (a, b) {
+  return b.arvosana - a.arvosana;
+});
+
+//console.log(movies);
+
+//
+
+for (const movie of movies) {
+  document.querySelector('#target').innerHTML +=
+    `<li>${movie.nimi}, ${movie.arvosana}</li>`;
+  console.log(document.querySelector('#target'));
+  //console.log(movie);
+}
+
+document
+  .querySelector('#suosikki')
+  .insertAdjacentHTML('afterbegin', movies[0].nimi);
