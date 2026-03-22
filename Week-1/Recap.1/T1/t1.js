7a41ecc22c2c4373d3169913892be27f0b74b270
Khaled <khaled-marai1@hotmail.com>
2026-03-22 17:57:11 +0200

    Add temperature converter program

--- Week-1/Recap.1/T1/t1.js
+++ Week-1/Recap.1/T1/t1.js
@@ -1,5 +1,10 @@
-function greeting() {
-  console.log('Hello, World!');
-}
+'use strict';
 
-greeting();
+const celsius = prompt('Enter temperature in Celsius:');
+const fahrenheit = ((celsius * 9) / 5 + 32).toFixed(2);
+const kelvin = (celsius + 273.15).toFixed(2);
+
+document.querySelector('#result').innerHTML = `Celsius: ${celsius} °C <br>
+    Fahrenheit: ${fahrenheit} °F <br>
+    Kelvin: ${kelvin} K
+    `;
