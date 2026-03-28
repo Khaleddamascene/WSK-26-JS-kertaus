33a5e9e91637fcabba86a24133abb4f6513faca7
Khaled <khaled-marai1@hotmail.com>
2026-03-28 22:45:34 +0200

    Refactor HTML structure and implement todo list rendering in JavaScript

--- Week-2/dom-assignments/t2/t2.js
+++ Week-2/dom-assignments/t2/t2.js
@@ -28,3 +28,26 @@ const todoList = [
 ];
 
 // add your code here
+
+// 1. Select the target ul element
+const targetList = document.querySelector('ul');
+
+todoList.forEach((todo) => {
+  const li = document.createElement('li');
+  const checkbox = document.createElement('input');
+  const label = document.createElement('label');
+
+  // 3. Configure the checkbox
+  checkbox.type = 'checkbox';
+  checkbox.id = `todo-${todo.id}`;
+  checkbox.checked = todo.completed;
+
+  // 4. Configure the label
+  label.htmlFor = `todo-${todo.id}`;
+  label.textContent = todo.task;
+
+  li.appendChild(checkbox);
+  li.appendChild(label);
+  // 6. Add the finished <li> to the <ul> on the page
+  targetList.appendChild(li);
+});
